717add8eba61ff2b356c36a27582cd10840e60f7
Afrina Parvin Moury <74785814+Afina-1696@users.noreply.github.com>
2023-11-20 22:52:43 +0600

    v1

--- Mern Project/backend/server.js
+++ Mern Project/backend/server.js
@@ -1,10 +1,19 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
-const { path } = require("./app");
+// const { path } = require("./app");
+const connectDatabase = require("./config/database")
 
 //Config
 dotenv.config({path:"backend/config/config.env"});
+
+
+//connecting to database
+connectDatabase();
+
+
+
+
 app.listen(process.env.PORT,()=>{
-    console.log('Server is working on http://localhost:${process.env.PORT}')
+    console.log(`Server is working on http://localhost:${process.env.PORT}`)
 })
