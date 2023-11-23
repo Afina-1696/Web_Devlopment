a7be8f4d0994daa17280a4d49a684cbab678439f
Afrina Parvin Moury <74785814+Afina-1696@users.noreply.github.com>
2023-11-24 00:40:49 +0600

    v4

--- Mern Project/backend/routes/productRoute.js
+++ Mern Project/backend/routes/productRoute.js
@@ -1,15 +1,41 @@
 const express = require("express");
-const {getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails} = require("../controllers/productController");
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
+  getAdminProducts,
+} = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
-
 router.route("/products").get(getAllProducts);
 
-router.route("/product/new").post(createProduct);
+router
+  .route("/admin/products")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
 
-router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails)
+router
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 
+router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
+router.route("/product/:id").get(getProductDetails);
 
-module.exports = router
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
+
+module.exports = router;
--- Mern Project/backend/server.js
+++ Mern Project/backend/server.js
@@ -1,19 +1,38 @@
 const app = require("./app");
+const cloudinary = require("cloudinary");
+const connectDatabase = require("./config/database");
 
-const dotenv = require("dotenv");
-// const { path } = require("./app");
-const connectDatabase = require("./config/database")
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
 
-//Config
-dotenv.config({path:"backend/config/config.env"});
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 
-
-//connecting to database
+// Connecting to database
 connectDatabase();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
 
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is working on http://localhost:${process.env.PORT}`)
-})
+  server.close(() => {
+    process.exit(1);
+  });
+});
