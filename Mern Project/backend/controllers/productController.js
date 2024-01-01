d9ee9f1f1d1f2ce237b83dca0ad2886a92ca9744
Afrina Parvin Moury <74785814+Afina-1696@users.noreply.github.com>
2024-01-01 12:49:02 +0600

    v1.1

--- Mern Project/backend/controllers/productController.js
+++ Mern Project/backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 //Create Product -- Admin
 exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
@@ -9,20 +10,20 @@ exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
 
     res.status(201).json({
         success:true,
-        product
-    })
+        product,
+    });
 
 });
 
 // Get All Product
 exports.getAllProducts = catchAsyncErrors(async(req,res)=>{
-
-    const products = await Product.find();
+    const apiFeature = new ApiFeatures(Product.find(),req.query).search();
+    const products = await apiFeature.query();
 
     res.status(200).json({
         success:true,
         products,
-    })
+    });
     
 });
 
